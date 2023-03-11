e6799ceefad158078e6e9029d2bb8c7b12700447
BHAVYAPODDAR <poddarbhavya3@gmail.com>
2023-03-11 18:05:42 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './pages/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Landing from './pages/Landing';
+import Dashboard from './pages/'
 
 function App() {
   return (
